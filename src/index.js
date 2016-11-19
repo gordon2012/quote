00d978ae390b00c077a6e169afcdbbb4c014d2c7
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-11-18 21:48:21 -0800

    Add styling with Sass (Closes #4)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducer';
 import {setState} from './action_creators';
-
 import App from './components/App';
+import './sass/style.sass';
 
 const store = createStore(reducer);
 
