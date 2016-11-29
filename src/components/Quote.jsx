4f167b0cbf40ccc646a90eb44de9307e2cf83e72
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-11-28 19:00:00 -0800

    Disable get quote button if there are no quotes buffered (Closes #18)

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -6,10 +6,11 @@ import Entry from './Entry';
 export const Quote = React.createClass({
     render: function() {
         const {list, quotes} = this.props;
+        const disabled = Object.keys(quotes).length > 0 ? '' : 'disabled';
 
         return <div className="quote">
             <header><h1>Random Quote Generator</h1></header>
-            <button onClick={this.props.addList}><h3>Get a Quote</h3></button>
+            <button disabled={disabled} onClick={disabled ? '' : this.props.addList}><h3>Get a Quote</h3></button>
 
             <div className="quotelist">
                 {list.map(e =>
