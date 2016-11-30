22d1f47b414142dedb433bca3e83c276cf3f9641
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-11-30 10:09:28 -0800

    Add footer (Closes #15)

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -8,15 +8,20 @@ export const Quote = React.createClass({
         const {list, quotes} = this.props;
         const disabled = Object.keys(quotes).length > 0 ? '' : 'disabled';
 
-        return <div className="quote">
+        return <div className="app">
             <header><h1>Random Quote Generator</h1></header>
-            <button disabled={disabled} onClick={disabled ? '' : this.props.addList}><h3>Get a Quote</h3></button>
+            <div className="content">
+                <div className="quote">
+                    <button disabled={disabled} onClick={disabled ? '' : this.props.addList}><h3>Get a Quote</h3></button>
 
-            <div className="quotelist">
-                {list.map(e =>
-                    <Entry key={e} quote={quotes[e]}></Entry>
-                )}
+                    <div className="quotelist">
+                        {list.map(e =>
+                            <Entry key={e} quote={quotes[e]}></Entry>
+                        )}
+                    </div>
+                </div>
             </div>
+            <footer><div><span>Copyright 2016 Gordon Doskas</span><span className="divider"> | </span><span>Powered by forismatic</span></div></footer>
         </div>;
     }
 });
